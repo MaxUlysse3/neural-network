c1bfbb3a1e50dbcd98aa76795685fe9d8ba0d54f
Ulysse <ulyssewaintal@gmail.com>
2024-02-02 12:38:12 +0100

    Stopped using 'rulinalg' create. Replaced by 'ndarray' crate.

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,19 @@
 
 use rand::Rng;
 use serde::{Serialize, Deserialize};
-use rulinalg::{
-    matrix::Matrix,
-    vector::Vector,
+use ndarray::{
+    Array1, Array2,
 };
 
+/// The struct representing a neural network.
+#[derive(Serialize, Deserialize)]
 pub struct Network {
-    weights: Vec<Matrix<f64>>,
-    biases: Vec<Vector<f64>>,
+    weights: Vec<Array2<f64>>,
+    biases: Vec<Array1<f64>>,
 }
 
 impl Network {
+    /// Return a new [`Network`] with no layer.
     pub fn new() -> Self {
         Self {
             weights: Vec::<_>::new(),
@@ -20,7 +22,9 @@ impl Network {
         }
     }
     
+    /// Add a layer to the [`Network`] with random weights and biases.
     pub fn add_layer_random(&mut self, input: usize, output: usize) {
-        self.weights.push(Matrix::from_fn(output, input, |_, _| <i32 as Into::<f64>>::into(rand::thread_rng().gen_range(0..1000)) / 1000f64));
+        self.weights.push(Array2::from_shape_fn((output, input), |(_, _)| <i32 as Into::<f64>>::into(rand::thread_rng().gen_range(0..1000)) / 1000f64));
+        self.biases.push(Array1::from_shape_fn(output, |_| <i32 as Into<f64>>::into(rand::thread_rng().gen_range(0..1000)) / 1000f64));
     }
 }
