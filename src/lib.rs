2a4c4d93517243938c0a21d6c6a3b2ce300eb15b
Ulysse <ulyssewaintal@gmail.com>
2024-02-02 13:37:49 +0100

    Added 'cost', 'sigma' and 'forward' functions. TODO : finish 'forward' function.

--- src/lib.rs
+++ src/lib.rs
@@ -27,4 +27,51 @@ impl Network {
         self.weights.push(Array2::from_shape_fn((output, input), |(_, _)| <i32 as Into::<f64>>::into(rand::thread_rng().gen_range(0..1000)) / 1000f64));
         self.biases.push(Array1::from_shape_fn(output, |_| <i32 as Into<f64>>::into(rand::thread_rng().gen_range(0..1000)) / 1000f64));
     }
+
+    /// Return all the activations of the layers.
+    ///
+    /// # Panic
+    ///
+    /// If the [`Network`] doesn't have any layer.
+    pub fn forward(&self, input: Array1<f64>) -> Vec<Array1<f64>> {
+        if self.weights.len() == 0 {
+            panic!("The Network doesn't habve any layer. Try adding one with 'add_layer_random.");
+        }
+
+        let mut acts = &input;
+        let mut to_return: Vec<_> = vec![];
+        for (w, b) in self.weights.iter().zip(self.biases.iter()) {
+            //let mut z: Array1<f64> = w * acts + b;
+            //z = Self::sigma(z);
+            acts = &z;
+            to_return.push(z);
+        }
+
+        to_return
+    }
+
+    /// The cost function (sum of squared error).
+    fn cost(out: &Vec<f64>, vals: &Vec<f64>) -> f64 {
+        let mut sum = 0f64;
+        for (i, j) in out.iter().zip(vals) {
+            sum += (j - i).powf(2f64);
+        }
+        sum
+    }
+
+    fn sigma(x: f64) -> f64 {
+        1f64 / (1.0 - libm::exp(x))
+    }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_cost() {
+        assert_eq!(Network::cost(&vec![0.0, 0.1, 0.5, 1.3], &vec![0.0, 0.1, 0.5, 1.3]), 0.0);
+    }
+}
+
+
